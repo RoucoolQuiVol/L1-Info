e9fa877e61e34f8840d07075c8d82c43efdbe2f6
Almir.P <almir.pasic70@gmail.com>
2026-05-08 14:16:38 +0200

    Add program to calculate the average of two numbers

---
+++ LIFAPI/TP01/Question Moyenne.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+
+    float Nb1, Nb2;
+
+    float Moyenne;
+
+    cout << "Veuillez saisir vos deux réels SVP" << endl;
+    cin >> Nb1 >> Nb2;
+
+    Moyenne = (Nb1 + Nb2) / 2;
+
+    cout << "La moyenne est : " << Moyenne << endl;
+
+    return 0;
+
+}
